aa8010269945b0ada25f89a7b024d017810cc568
igor Viotto <igorcviotto@gmail.com>
2025-04-26 16:21:24 -0300

    Revert "Primeiro commit"
    This reverts commit 9b82cd332f6ef73f05e4aa402560f2be6178d91b.

--- src/main/java/com/igor/garantias/enums/StatusProposta.java
+++
@@ -1,8 +0,0 @@
-package com.igor.garantias.enums;
-
-public enum StatusProposta {
-    EM_ABERTO,
-    ACEITA,
-    RECUSADA,
-    CANCELADA
-}
--- src/main/java/com/igor/garantias/exception/CepNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.igor.garantias.exception;
-
-public class CepNaoEncontradoException extends RuntimeException {
-
-    public CepNaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-}
